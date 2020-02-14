598a5aceb876dbec764b569cb82276a4dd31db7d
denitsapeeva <60873960+denitsapeeva@users.noreply.github.com>
2020-02-14 14:14:33 +0200

    Update student.py

--- application/student.py
+++ application/student.py
@@ -3,8 +3,10 @@ class Students:
     self.name_of_student = name_of_student;
     self.year = years;
 
-  def get_student(self):
+  def get_graduates(self):
     return self.name_of_student + " will be "+str(4+self.year)+" years old, when he graduates!"
   def years_of_student(self):
     print (self.name_of_student + " is " + str(self.year) + " years old!")
+
+
 
